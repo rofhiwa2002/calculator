fea7b4f283bd2423f5c59a93f057414b5b694c38
rofhiwa2002 <35018690+rofhiwa2002@users.noreply.github.com>
2025-03-30 14:40:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//I got this from chatGpt and broCode
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        alert('Invalid Expression');
+    }
+}
+
